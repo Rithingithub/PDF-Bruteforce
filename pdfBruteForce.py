11e80419165670e81a3320d4ad54c86c2885ffea
Rithin Lehan <74766580+Rithingithub@users.noreply.github.com>
2021-12-09 09:06:40 +0530

    Create pdfBruteForce.py

---
+++ pdfBruteForce.py
@@ -0,0 +1,23 @@
+import pikepdf
+from tqdm import tqdm
+
+print("""   
+ ____  ____  _    _____ _____      _____ ____  ____  ____  _____
+/  __\/  __\/ \ /Y__ __Y  __/     /    //  _ \/  __\/   _\/  __/
+| | //|  \/|| | || / \ |  \ _____ |  __\| / \||  \/||  /  |  \  
+| |_\\|    /| \_/| | | |  /_\____\| |   | \_/||    /|  \__|  /_ 
+\____/\_/\_\\____/ \_/ \____\     \_/   \____/\_/\_\\____/\____\
+                                                                    
+""")
+passwords = [line.strip() for line in open("wordlist.txt")]
+
+a = input(print("Paste the pdf file directory : "))
+
+for password in tqdm(passwords, "Decrypting PDF"):
+    try:
+        with pikepdf.open(a, password=password) as pdf:
+            print("\n[+] Password found  : ", password)
+            break
+    except pikepdf._qpdf.PasswordError as e:
+
+        continue
